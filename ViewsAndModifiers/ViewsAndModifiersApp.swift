5b0e83912d0248b226e169477f5aefe465cc91fb
Devy <izabela.marcinkowska@outlook.com>
2024-08-28 10:17:52 +0200

    First commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Izabela Marcinkowska on 2024-08-28.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
